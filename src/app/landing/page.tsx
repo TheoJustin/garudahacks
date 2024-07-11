b204f818f786e9c76c8f01f40cc13bbab21f396f
Darryleffendi <darrylgate@gmail.com>
2024-07-11 22:41:24 +0700

    feat: low level landing page, navigation fix

--- src/app/landing/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LandingPage() {
-    return (
-        <div>
-            Test
-        </div>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+
+
+import { Button } from "@/lib/components/ui/button";
 import Image from "next/image";
+import CtaButton from "./_components/cta-button";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <link href={"/landing"}>Landing Page</link>
+    <main className="w-screen h-screen flex flex-col">
+
+      {/* Heading */}
+      <header>
+        <h1>Land Your Dream Job with Ease and Confidence</h1>
+        <h3>With AI-driven insights and personalized recommendations to guide your career journey</h3>
+      </header>
+      
+      {/* CTA Button */}
+      <CtaButton />
+
     </main>
   );
 }
